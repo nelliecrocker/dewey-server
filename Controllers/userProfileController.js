aa6b5858c8abc77b1589bcd538d185a3553a8a91
Nellie <80473940+nelliecrocker@users.noreply.github.com>
2021-09-14 16:29:19 -0400

    all profile endpoints working

--- Controllers/userProfileController.js
+++ Controllers/userProfileController.js
@@ -27,21 +27,82 @@ router.post("/create", validateJWT, async (req, res) => {
 })
 
 // console.log("********************")
-//get all profiles
-router.get('/user/:id', validateJWT, async (req, res) => {
+router.get("/allprofiles", async (req, res) => {
     try {
-        const user = await User.findOne({
-            where: ({id: req.params.id}),
-            include: UserProfile
-        })
-    }catch (err) {
+        const allProfiles = await UserProfile.findAll()
+        if (allProfiles) {
+            res.status(200).json(allProfiles)
+        } else throw "unable to find any profiles"
+    } catch (err) {
         res.status(500).json({ error: err })
     }
 })
 
-//update profile
 
-//delete profile
+router.get("/myprofile", validateJWT, async (req, res) => {
+    const { id } = req.user
+    try {
+        const userProfile = await UserProfile.findAll({
+            where: {
+                UserId: id
+            }
+        })
+        if (userProfile) {
+            res.status(200).json(userProfile)
+        } else throw "unable to find your profile"
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
+router.put("/update/:profileId", validateJWT, async (req, res) => {
+
+    const { preferredGenre, favoriteCharacter, collectionSize } = req.body.UserProfile
+
+    const profileId = req.params.profileId
+    const userId = req.user.id
+    try {
+        const query = {
+            where: {
+                id: profileId,
+                UserId: userId
+            },
+            returning: true
+        }
+
+        const updatedProfile = {
+            preferredGenre: preferredGenre,
+            favoriteCharacter: favoriteCharacter,
+            collectionSize: collectionSize
+        }
+
+        const update = await UserProfile.update(updatedProfile, query)
+
+        if (update) {
+            res.status(200).json(update)
+        } else throw "unable to update this profile"
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
+router.delete("/delete/:profileId", validateJWT, async (req, res) => {
+    const profileId = req.params.profileId
+    const userId = req.user.id
+
+    try {
+        const query = {
+            where: {
+                id: profileId,
+                UserId: userId
+            }
+        }
+        await UserProfile.destroy(query)
+        res.status(200).json({ message: "Profile Removed" })
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
 
 
 
