596b427207f0eef283ced0afbadf451bd813e3c8
Nellie <80473940+nelliecrocker@users.noreply.github.com>
2021-09-30 20:38:05 -0400

    x

--- db.js
+++ db.js
@@ -2,11 +2,9 @@ const { Sequelize } = require('sequelize');
 // const { options } = require('./middleware/validate-jwt');
 
 const sequelize = new Sequelize(
-    process.env.DB_DBNAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DATABASE_URL,
     {
-        // host: process.env.DB_HOST,
+            host: process.env.DB_HOST,
         dialect: 'postgres',
         //comment out dialect options if work on local host
         dialectOptions: {
@@ -18,7 +16,7 @@ const sequelize = new Sequelize(
     });
 
 
-async function syncDb(sequelize, options){
+async function syncDb(sequelize, options) {
     const { force, alter } = options
     try {
         if (force)
@@ -35,9 +33,9 @@ async function syncDb(sequelize, options){
 //error handling for admin/role
 function errorHandler(err, req, res, next) {
     if (typeof (err) === 'string') {
-        return res.status(400).json({message:'Invalid token'})
+        return res.status(400).json({ message: 'Invalid token' })
     }
-    return res.status(500).json({message: err.message})
+    return res.status(500).json({ message: err.message })
 }
 
 
